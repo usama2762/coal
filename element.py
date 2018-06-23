af7c2a062dd53653fa84c9ea5aa796fe0050cb74
Usama <usamajamil77@gmail.com>
2018-06-24 00:05:53 +0500

    libraries

---
+++ element.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.support.ui import WebDriverWait
+import selenium.webdriver.support.ui as ui
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+import time
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
